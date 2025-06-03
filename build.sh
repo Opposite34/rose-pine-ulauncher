231c94571c46c0875dba62d1594285e4e58f23e8
Opposite34 <54463205+Opposite34@users.noreply.github.com>
2025-06-03 22:05:26 +0900

    removed generated.css, simplified build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
 npx @rose-pine/build@latest -t templates/theme.css -o ./
+npx @rose-pine/build@latest -t templates/manifest.json -o ./
 for file in rose-pine*.css; do
   mv "$file" "./dist/${file%.css}/theme.css"
-  cp "./templates/theme-gtk-3.20.css" "./dist/${file%.css}/theme-gtk-3.20.css"
+  mv "${file%.css}.json" "dist/${file%.css}/manifest.json"
+  cp "templates/theme-gtk-3.20.css" "dist/${file%.css}/"
 done
-
-npx @rose-pine/build@latest -t templates/manifest.json -o ./
-for file in rose-pine*.json; do
-  mv "$file" "./dist/${file%.json}/manifest.json"
-done
-
-npx @rose-pine/build@latest -t templates/generated.css -o ./
-for file in rose-pine*.css; do
-  mv "$file" "./dist/${file%.css}/generated.css"
-done
-
